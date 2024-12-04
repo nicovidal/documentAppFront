2d7665446176a1dd407d2d9e89bb1600dbc6870f
nicovidal <nico.vidal.m@gmail.com>
2024-12-04 17:01:47 -0300

    cambio

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')  
+    }
+  }
 })
